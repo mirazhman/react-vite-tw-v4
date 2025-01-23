7d56657247dbcb0769cd49e7260f1bc9f1f94d7c
mirazhman <mirazhran778@gmail.com>
2025-01-23 13:09:18 +0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react"
-
+import NavBar from './components/NavBar'
+import Home from './components/Home'
 function App() {
 
 
   return (
     <>
-      <div className="bg-amber-500 mt-20 text-6xl text-center">Hello Tailwind v4 </div>
+      <NavBar />
+        <div className="bg-amber-500 mt-20 text-6xl text-center">Hello Tailwind v4 </div>
+      <Home />
     </>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>Navigations Header</div>
+  )
+}
+
+export default NavBar
