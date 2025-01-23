ff168dba611a07f18b39ff62b92c8ac3fb24a0e6
mirazhman <mirazhran778@gmail.com>
2025-01-23 10:11:44 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-amber-500 mt-20 text-6xl text-center">Hello Tailwind v4 </div>
+    </>
+  )
+}
+
+export default App
